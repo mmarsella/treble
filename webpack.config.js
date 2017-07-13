961e5dea9d449b078d759f957262b69ce0c55ad8
Mark Marsella <mark@givingassistant.org>
2017-07-12 17:23:22 -0700

    sass loader configured in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,14 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass']
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   },
