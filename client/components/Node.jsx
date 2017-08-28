b81a8d6a64ad341be1b9b617b06863f36f517836
Samuel Hastings <shastings517@gmail.com>
2017-08-27 19:21:13 -0700

    replace dash with image

--- client/components/Node.jsx
+++ client/components/Node.jsx
@@ -20,26 +20,24 @@ class Node extends React.Component{
 				// debugger
 				return(
 					<div className='node'>
-					<form onSubmit={this.props.handleSubmit} className="nodeForm" name='nodeForm'>
-						<input
-							ref={(ip)=> this.myInput= ip}
-							name={this.props.stringNumber + '-' + this.props.idx} 
-							className="tabInput"
-							onChange={this.props.handleChange}
-							value={this.props.val}
-							onKeyDown={this.props.onKeyDown}
-						/>
-					</form>
-				</div>
+						<form onSubmit={this.props.handleSubmit} className="nodeForm" name='nodeForm'>
+							<input
+								ref={(ip)=> this.myInput= ip}
+								name={this.props.stringNumber + '-' + this.props.idx} 
+								className="tabInput"
+								onChange={this.props.handleChange}
+								value={this.props.val}
+								onKeyDown={this.props.onKeyDown}
+							/>
+						</form>
+					</div>
 				)
 			}
 		
 			return(
 
-				<div className='node' onClick={this.props.handleClick}
-				ref={(ip)=> this.myNode= ip}
-				>
-					{this.props.val ? this.props.val: '-'}
+				<div className='node' onClick={this.props.handleClick} ref={(ip)=> this.myNode= ip}>
+					{this.props.val ? this.props.val: <img src='../assets/dash.png' />}
 				</div>
 			)
 	}
