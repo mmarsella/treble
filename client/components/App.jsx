10754271b9848240ad9bd1b1990d689a18a14121
Mark Marsella <mmarsella114@g.rwu.edu>
2017-09-12 18:24:35 -0700

    add notes to App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,14 +8,49 @@ import Navbar from './navbar/navbar.jsx';
 export default class App extends React.Component {
 
   // Login logic shoudl bubble up to the App level from the navbar
+
+  // No User
+  /*  Intro Screen  -- need this compoennt */
   
+  // Logged In
+  /* <CompositionList></CompositionList> */
+
+  // On Select of Composition (in CompositionList)
+  /* <Composition> */
+
   // handleChange(e){
   //   console.log('e.target:', e.target);
   // }
 
-  // signUp(e){
 
-  // }
+
+  // Should only trigger login.... for now
+
+  // Should return a User object
+
+  // {email,uid}
+  handleSubmit(e){
+
+  }
+
+
+
+  // Both login and signup will change this.state.User --> this triggers a re-render.  Render components accordingly
+  signUp(e){
+
+  }
+
+  // Only supporting login right now
+  login(e){
+
+  }
+
+
+  // If we refresh - checks localstorage here to determine if there is a User obj or not.  
+  // If there is a User object - render Components accordingly
+  componentWillMount(){
+
+  }
 
 
   render() {
