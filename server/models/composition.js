b1bd446ef2eb0673f3b2d3fd59126734f22368b3
Mark Marsella <mmarsella114@g.rwu.edu>
2017-10-03 17:06:49 -0700

    edit composition

--- server/models/composition.js
+++ server/models/composition.js
@@ -4,8 +4,8 @@ var User = require("./user");
 var compSchema = new mongoose.Schema({
       data: { type: String},
       name: { type: String},
-       uid: { type: String},
-       user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }  //may want to look into referencing the User Schema
+       // uid: { type: String}, --> user
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }  //may want to look into referencing the User Schema
 });
 
 var Composition = mongoose.model('Composition', compSchema);
